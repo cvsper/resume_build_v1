fc36008bef51f25cb8eac2cd199a5ee2d7e3bffd
shamar donaldson <sevs@macbookpro.mynetworksettings.com>
2025-06-05 08:16:33 -0400

    styling bug fixes

--- static/js/main.js
+++ static/js/main.js
@@ -16,14 +16,12 @@
   window.onscroll = function () {
     const header_navbar = document.querySelector(".navbar-area");
     const sticky = header_navbar.offsetTop;
-    const logo = document.querySelector(".navbar-brand img");
-
-    if (window.pageYOffset > sticky) {
+    const logo = document.querySelector(".navbar-brand img");    if (window.pageYOffset > sticky) {
       header_navbar.classList.add("sticky");
-      logo.src = "{{ url_for('static', filename='img/logo/logo-2.svg') }}";
+      logo.src = "/static/img/logo/logo-2.svg";
     } else {
       header_navbar.classList.remove("sticky");
-      logo.src = "{{ url_for('static', filename='img/logo/logo.svg') }}";
+      logo.src = "/static/img/logo/logo.svg";
     }
 
     // show or hide the back-top-top button
